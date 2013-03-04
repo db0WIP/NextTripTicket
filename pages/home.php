e93c1e25cff335e2841218194a271a79181342c4
db0company <db0company@gmail.com>
2013-03-04 23:09:14 +0100

    First simple version

---
+++ pages/home.php
@@ -0,0 +1,13 @@
+<div class="row-fluid">
+  <div class="span4"><div class="ticket">Paris, France</div></div>
+  <div class="span4"><div class="ticket">New York, USA</div></div>
+  <div class="span4"><div class="ticket">San Francisco, USA</div></div>
+</div>
+
+<div class="row-fluid">
+  <div class="span4"><div class="ticket">Random ticket</div></div>
+  <div class="span4"><div class="ticket">Search</div></div>
+  <div class="span4"><div class="ticket">Submit your own ticket</div></div>
+</div>
+
+
